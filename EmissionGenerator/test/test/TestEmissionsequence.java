535ef659b691185e21b410b89275ede2c4ba93f2
Sebastian <Sebastian@X220>
2013-04-12 14:48:05 +0200

    implement test cases for the emissionsequence class

---
+++ EmissionGenerator/test/test/TestEmissionsequence.java
@@ -0,0 +1,97 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.LinkedList;
+
+import org.junit.Test;
+
+import data.Emissionsequence;
+
+/**
+ * @author Sebastian
+ * 
+ */
+public class TestEmissionsequence {
+
+	/**
+	 * Test method for {@link data.Emissionsequence#Emissionsequence(String)}.
+	 */
+	@Test
+	public void testEmissionsequence() {
+		// set up SUT
+		String emissionString = "\"1\",\"2\",\"3\"\n";
+		// execute SUT
+		Emissionsequence emisSeq = new Emissionsequence(emissionString);
+		// verify outcome
+		assertEquals(emissionString, emisSeq.toString());
+		// tear down
+
+	}
+
+	/**
+	 * Test method for {@link data.Emissionsequence#getEmissionsAsArray()}.
+	 */
+	@Test
+	public void testGetEmissionsAsArray() {
+		// set up SUT
+		double[] emissionArray;
+		LinkedList<Integer> sequence = new LinkedList<>();
+		sequence.add(1);
+		sequence.add(2);
+		sequence.add(3);
+
+		// execute SUT
+		Emissionsequence emisSeq = new Emissionsequence(sequence);
+		emissionArray = emisSeq.getEmissionsAsArray();
+		// verify outcome
+		assertEquals(emissionArray.length, sequence.size());
+		// tear down
+
+	}
+
+	/**
+	 * Test method for {@link data.Emissionsequence#getAbsMatrix()}.
+	 */
+	@Test
+	public void testGetAbsMatrix() {
+		// set up SUT
+		LinkedList<Integer> sequence = new LinkedList<>();
+		sequence.add(0);
+		sequence.add(0);
+		sequence.add(1);
+
+		// execute SUT
+		Emissionsequence emisSeq = new Emissionsequence(sequence);
+		emisSeq.createMatrix();
+		int firstAbs = (int) emisSeq.getAbsMatrix().getValueAt(0, 1);
+		// verify outcome
+		assertEquals(firstAbs, 1);
+		// tear down
+
+	}
+
+	/**
+	 * Test method for {@link data.Emissionsequence#getRelMatrix()}.
+	 */
+	@Test
+	public void testGetRelMatrix() {
+		// set up SUT
+		LinkedList<Integer> sequence = new LinkedList<>();
+		sequence.add(0);
+		sequence.add(0);
+		sequence.add(1);
+
+		// execute SUT
+		Emissionsequence emisSeq = new Emissionsequence(sequence);
+		emisSeq.createMatrix();
+
+		// to avoid assertEquals with doubles
+		int firstRel = (int) ((double) (emisSeq.getRelMatrix().getValueAt(0, 1)) * 10);
+
+		// verify outcome
+		assertEquals(firstRel, 5);
+		// tear down
+
+	}
+}
